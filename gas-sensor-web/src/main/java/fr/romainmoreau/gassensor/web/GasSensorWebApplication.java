5b8df7ce369effa0e613428da661fc3286c863bc
Romain Moreau <romainmoreau@gmail.com>
2018-05-19 10:19:40 +0200

    Fix deprecation

--- gas-sensor-web/src/main/java/fr/romainmoreau/gassensor/web/GasSensorWebApplication.java
+++ gas-sensor-web/src/main/java/fr/romainmoreau/gassensor/web/GasSensorWebApplication.java
@@ -2,13 +2,13 @@ package fr.romainmoreau.gassensor.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @SpringBootApplication
 @EnableWebSocketMessageBroker
-public class GasSensorWebApplication extends AbstractWebSocketMessageBrokerConfigurer {
+public class GasSensorWebApplication implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/stompEndpoint").withSockJS();
