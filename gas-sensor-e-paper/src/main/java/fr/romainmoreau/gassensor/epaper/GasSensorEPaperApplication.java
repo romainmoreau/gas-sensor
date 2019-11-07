c21319551b94476fd050033d86d37520f7edca22
Romain Moreau <romainmoreau@gmail.com>
2019-11-07 12:23:23 +0100

    Spring Boot upgrade

--- gas-sensor-e-paper/src/main/java/fr/romainmoreau/gassensor/epaper/GasSensorEPaperApplication.java
+++ gas-sensor-e-paper/src/main/java/fr/romainmoreau/gassensor/epaper/GasSensorEPaperApplication.java
@@ -2,10 +2,12 @@ package fr.romainmoreau.gassensor.epaper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GasSensorEPaperApplication {
 	public static final void main(String[] args) {
 		SpringApplication.run(GasSensorEPaperApplication.class, args);
--- gas-sensor-web/src/main/java/fr/romainmoreau/gassensor/web/GasSensorWebApplication.java
+++ gas-sensor-web/src/main/java/fr/romainmoreau/gassensor/web/GasSensorWebApplication.java
@@ -3,6 +3,7 @@ package fr.romainmoreau.gassensor.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,6 +13,7 @@ import fr.romainmoreau.gassensor.datamodel.GasSensorDataModel;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = GasSensorDataModel.class)
+@ConfigurationPropertiesScan
 @EnableScheduling
 @EnableWebSocketMessageBroker
 public class GasSensorWebApplication implements WebSocketMessageBrokerConfigurer {
